599f5fcdddcb50a7349efa703abae1a46f292610
Moises RZ <loveloungeoficial@gmail.com>
2021-02-03 16:30:25 -0300

    refactoring hotel class DDD model

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-
+from models.hotel import HotelModel
 hoteis = [{
     'hotel_id': 'alpha',
     'nome': 'Alpha Hotel',
@@ -21,6 +21,8 @@ hoteis = [{
 }
 
 ]
+
+
 
 
 class Hoteis(Resource):
@@ -48,19 +50,17 @@ class Hotel(Resource):
         return {'message': 'Hotel not found.'}, 404  # not found
 
     def post(self, hotel_id):
-
         dados = Hotel.argumentos.parse_args()
-
-        novo_hotel = {'hotel_id': hotel_id, **dados}
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
         hoteis.append(novo_hotel)
         return novo_hotel, 200
 
     def put(self, hotel_id):
 
         dados = Hotel.argumentos.parse_args()
-
-        novo_hotel = {'hotel_id': hotel_id, **dados}
-
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
         hotel = Hotel.find_hotel(hotel_id)
         if hotel:
             hotel.update(novo_hotel)
