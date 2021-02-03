491779dfd8f90cc617ade946040d0c5018266d56
Moises RZ <loveloungeoficial@gmail.com>
2021-02-03 04:13:16 -0300

    Create PUT Method and Refactor the POST Method

--- app.py
+++ app.py
@@ -1,38 +1,14 @@
 from flask import Flask
-from flask_restful import Resource, Api
-
+from flask_restful import Api
+from resources.hotel import Hoteis, Hotel
 
 app = Flask(__name__)
 api = Api(app)
 
-hoteis = [{
-    'hotel_id': 'alpha',
-    'nome': 'Alpha Hotel',
-    'estrelas': 4.5,
-    'diaria': 420.34,
-    'cidade': 'Rio de Janeiro'
-    },{
-    'hotel_id': 'bravo',
-    'nome': 'Bravo Hotel',
-    'estrelas': 4.4,
-    'diaria': 420.34,
-    'cidade': 'Santa Catarina'
-    },{
-    'hotel_id': 'charlie',
-    'nome': 'Charlie Hotel',
-    'estrelas': 3.9,
-    'diaria': 420.34,
-    'cidade': 'Santa Catarina'
-    }
-
-]
-
- 
-class Hoteis(Resource):
-    def get(self):
-        return {'hoteis': hoteis}
 
 api.add_resource(Hoteis, '/hoteis')
+api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
---
+++ resources/hotel.py
@@ -0,0 +1,72 @@
+from flask_restful import Resource, reqparse
+
+hoteis = [{
+    'hotel_id': 'alpha',
+    'nome': 'Alpha Hotel',
+    'estrelas': 4.5,
+    'diaria': 420.34,
+    'cidade': 'Rio de Janeiro'
+}, {
+    'hotel_id': 'bravo',
+    'nome': 'Bravo Hotel',
+    'estrelas': 4.4,
+    'diaria': 420.34,
+    'cidade': 'Santa Catarina'
+}, {
+    'hotel_id': 'charlie',
+    'nome': 'Charlie Hotel',
+    'estrelas': 3.9,
+    'diaria': 420.34,
+    'cidade': 'Santa Catarina'
+}
+
+]
+
+
+class Hoteis(Resource):
+    def get(self):
+        return {'hoteis': hoteis}
+
+
+class Hotel(Resource):
+    argumentos = reqparse.RequestParser()
+    argumentos.add_argument('nome')
+    argumentos.add_argument('estrelas')
+    argumentos.add_argument('diaria')
+    argumentos.add_argument('cidade')
+
+    def find_hotel(hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+        return None
+
+    def get(self, hotel_id):
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            return hotel
+        return {'message': 'Hotel not found.'}, 404  # not found
+
+    def post(self, hotel_id):
+
+        dados = Hotel.argumentos.parse_args()
+
+        novo_hotel = {'hotel_id': hotel_id, **dados}
+        hoteis.append(novo_hotel)
+        return novo_hotel, 200
+
+    def put(self, hotel_id):
+
+        dados = Hotel.argumentos.parse_args()
+
+        novo_hotel = {'hotel_id': hotel_id, **dados}
+
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            hotel.update(novo_hotel)
+            return novo_hotel, 200  # ok
+        hoteis.append(novo_hotel)
+        return novo_hotel, 201  # created criado
+
+    def delete(self, hotel_id):
+        pass
