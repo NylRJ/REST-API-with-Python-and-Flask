cc56da47811ab84b0441b28ffe29be9c2751c2f2
Moises RZ <loveloungeoficial@gmail.com>
2021-02-03 03:09:55 -0300

    method post

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+
+app = Flask(__name__)
+api = Api(app)
+
+hoteis = [{
+    'hotel_id': 'alpha',
+    'nome': 'Alpha Hotel',
+    'estrelas': 4.5,
+    'diaria': 420.34,
+    'cidade': 'Rio de Janeiro'
+    },{
+    'hotel_id': 'bravo',
+    'nome': 'Bravo Hotel',
+    'estrelas': 4.4,
+    'diaria': 420.34,
+    'cidade': 'Santa Catarina'
+    },{
+    'hotel_id': 'charlie',
+    'nome': 'Charlie Hotel',
+    'estrelas': 3.9,
+    'diaria': 420.34,
+    'cidade': 'Santa Catarina'
+    }
+
+]
+
+ 
+class Hoteis(Resource):
+    def get(self):
+        return {'hoteis': hoteis}
+
+api.add_resource(Hoteis, '/hoteis')
+
+if __name__ == '__main__':
+    app.run(debug=True)
