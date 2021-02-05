8f84060cc8f911668a74d4dda2038017db0cbb24
Moises RZ <loveloungeoficial@gmail.com>
2021-02-05 00:27:16 -0300

    Add ORM sql_alchemy

--- models/hotel.py
+++ models/hotel.py
@@ -1,5 +1,15 @@
+from sql_alchemy import banco
 
-class HotelModel:
+
+class HotelModel(banco.Model):
+    __tablename__ = 'hoteis'
+
+    hotel_id = banco.Column(banco.String, primary_key=True)
+    nome = banco.Column(banco.String(80))
+    estrelas = banco.Column(banco.Float(precision=1))
+    diaria = banco.Column(banco.Float(precision=2))
+    cidade = banco.Column(banco.String(40))
+
     def __init__(self, hotel_id, nome, estrelas, diaria, cidade):
         self.hotel_id = hotel_id
         self.nome = nome
@@ -15,3 +25,7 @@ class HotelModel:
             'diaria': self.diaria,
             'cidade': self.cidade
         }
+
+    @classmethod
+    def find_hotel(cls, hotel_id):
+        hotel = cls.query.filter_by(hotel_id=hotel_id).first()
--- resources/hotel.py
+++ resources/hotel.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
+
 hoteis = [{
     'hotel_id': 'alpha',
     'nome': 'Alpha Hotel',
@@ -23,8 +24,6 @@ hoteis = [{
 ]
 
 
-
-
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
@@ -37,12 +36,6 @@ class Hotel(Resource):
     argumentos.add_argument('diaria')
     argumentos.add_argument('cidade')
 
-    def find_hotel(hotel_id):
-        for hotel in hoteis:
-            if hotel['hotel_id'] == hotel_id:
-                return hotel
-        return None
-
     def get(self, hotel_id):
         hotel = Hotel.find_hotel(hotel_id)
         if hotel:
@@ -50,6 +43,9 @@ class Hotel(Resource):
         return {'message': 'Hotel not found.'}, 404  # not found
 
     def post(self, hotel_id):
+        if HotelModel.find_hotel(hotel_id):
+            return {'message': 'hotel_id "{}" already exists.'.format(hotel_id)}, 400  # Bad request
+
         dados = Hotel.argumentos.parse_args()
         hotel_objeto = HotelModel(hotel_id, **dados)
         novo_hotel = hotel_objeto.json()
